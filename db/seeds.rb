97f3600f0d77b8b11431f0729b951c47438723dc
Ashley Hutchinson <hutchski@Ashleys-MBP.fios-router.home>
2021-04-06 00:45:38 -0400

    backend redeploy with new seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -10,8 +10,9 @@ Comment.destroy_all
 Post.destroy_all
 User.destroy_all
 
-@hutchski = User.create!(username: "hutchski", password: "password", user_photo: "https://i.imgur.com/Ev5rvQ8.jpg")
-@stokedog = User.create!(username: "stokedog", password: "password", user_photo: "https://i.imgur.com/ZsqYhLM.jpg?1")
+@hutchski = User.create!(username: "hutchski", password: "password", user_photo: "https://i.imgur.com/eEETwAK.jpg?1")
+@stokedog = User.create!(username: "stokedog", password: "password", user_photo: "https://i.imgur.com/H7XlWNY.jpg?1")
+@talubi = User.create!(username: "talubi", password: "password", user_photo: "https://i.imgur.com/HiLWMsK.jpg?1")
 
 @post1 = Post.create!(
     title: "huckleberry gates",
@@ -19,26 +20,35 @@ User.destroy_all
     distance: "18mi",
     duration: "4hrs 17m",
     elevation_gain: "19,000ft",
-    activity_photo: "https://i.imgur.com/1XoW1SZ.jpg",
+    activity_photo: "https://i.imgur.com/bxcAoyx.jpg",
     user: @hutchski
 )
 @post2 = Post.create!(
-    title: "corral",
-    activity_type: "bike",
-    distance: "7mi",
-    duration: "48m",
-    elevation_gain: "1240ft",
-    activity_photo: "https://i.imgur.com/dmzbkhv.jpg",
-    user: @hutchski
-)
-@post3 = Post.create!(
     title: "joggin' with my hooman",
     activity_type: "run",
     distance: "4.7mi",
     duration: "58m",
     elevation_gain: "62ft",
-    activity_photo: "https://i.imgur.com/j9u786i.jpg",
+    activity_photo: "https://i.imgur.com/v3Jm4rd.jpg",
     user: @stokedog
+)
+@post3 = Post.create!(
+    title: "slickrock",
+    activity_type: "bike",
+    distance: "9mi",
+    duration: "48m",
+    elevation_gain: "740ft",
+    activity_photo: "https://i.imgur.com/eKJp77f.jpg",
+    user: @hutchski
+)
+@post4 = Post.create!(
+    title: "borkin'",
+    activity_type: "run",
+    distance: "11.2mi",
+    duration: "2hr 58m",
+    elevation_gain: "1262ft",
+    activity_photo: "https://i.imgur.com/Vd5Paso.jpg?1",
+    user: @talubi
 )
 
 @comment1 = Comment.create!(
@@ -47,7 +57,17 @@ User.destroy_all
   post: @post3
 )
 @comment2 = Comment.create!(
-  content: "Gate 3 is my fave!",
+  content: "Gate 4 is my fave!",
+  user: @talubi,
+  post: @post1
+)
+@comment3 = Comment.create!(
+  content: "pshh! no way, gate 2 all day!",
   user: @stokedog,
   post: @post1
+)
+@comment4 = Comment.create!(
+  content: "What are you doing in Utah?!",
+  user: @talubi,
+  post: @post3
 )
